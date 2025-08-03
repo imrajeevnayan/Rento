8cdec323d0b144667de478844d4bfb5e18b5fd0d
imrajeevnayan <immrrobot121@gmail.com>
2025-08-03 14:19:25 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Rento/',  // 👈 add this for GitHub Pages deployment
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
